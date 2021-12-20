fc8e15edda18a85a5661e14520ab3dec54d1812d
Alexander Sulim <hello@sul.im>
2021-12-20 11:54:53 +0100

    Add proper start/stop/pause implementation

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 // Start/stop the timer.
 input.onButtonPressed(Button.A, function () {
     if (state == 0) {
+        basic.clearScreen()
         state = 1
-        started_at = control.millis()
-        current_min = 0
+        prev_tick_at = control.millis()
+        elapsed_msec = 0
+        elapsed_min = 0
     } else if (state == 1) {
         state = 2
     } else {
         state = 1
+        prev_tick_at = control.millis()
     }
 })
 // Toggle/show the current mode.
@@ -21,13 +24,16 @@ input.onButtonPressed(Button.AB, function () {
 // Reset the timer.
 input.onButtonPressed(Button.B, function () {
     state = 0
+    basic.showIcon(IconNames.No)
+    basic.pause(500)
     basic.clearScreen()
 })
 let y = 0
 let x = 0
-let delta_min = 0
-let current_min = 0
-let started_at = 0
+let elapsed_min_now = 0
+let elapsed_min = 0
+let prev_tick_at = 0
+let elapsed_msec = 0
 let state = 0
 let mode = 0
 // Modes:
@@ -39,20 +45,23 @@ mode = 0
 // 1 - started
 // 2 - paused
 state = 0
-started_at = 0
-let delta = 0
+elapsed_msec = 0
+prev_tick_at = 0
 basic.forever(function () {
     if (state == 1) {
-        delta = control.millis() - started_at
-        delta_min = Math.floor(delta / 1000)
-        if (delta_min > current_min) {
-            current_min = delta_min
-            x = Math.floor((current_min - 1) / 5)
-            y = (current_min - 1) % 5
+        elapsed_msec = elapsed_msec + (control.millis() - prev_tick_at)
+        prev_tick_at = control.millis()
+        elapsed_min_now = Math.floor(elapsed_msec / 1000)
+        if (elapsed_min_now > elapsed_min) {
+            elapsed_min = elapsed_min_now
+            x = Math.floor((elapsed_min - 1) / 5)
+            y = (elapsed_min - 1) % 5
             led.plot(x, y)
         }
-        if (current_min > 25) {
+        if (elapsed_min > 25) {
             state = 0
+            basic.showIcon(IconNames.Yes)
+            basic.pause(500)
             basic.clearScreen()
         }
     }
